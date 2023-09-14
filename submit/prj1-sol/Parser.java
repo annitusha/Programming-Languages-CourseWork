0c21003080b61622795036714c6dd6c3ee61d211
Anna Kozina <akozina2@cs.binghamton.edu>
2023-09-14 16:37:40 -0400

    started lab3

--- submit/prj1-sol/Parser.java
+++ submit/prj1-sol/Parser.java
@@ -1,14 +1,268 @@
 
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+import org.json.simple.JSONObject;
 import java.util.*;
 
-import oracle.jvm.hotspot.jfr.ThreadStates.States;
+class Token {
+    public String kind;
+    public String lexeme;
+    public Token(String kind, String lexeme){
+        this.kind = kind;
+        this.lexeme = lexeme;
+    }
+    public String toString() {
+        return "Token{" +
+                "type=" + kind +
+                ", value='" + lexeme + '\'' +
+                '}';
+    }
+}
 
 public class Parser {
 
+//EBNF grammar for the declrations language
+/*
+    program
+    : declaration program
+    | #empty
+    ;
 
+    declaration
+    : declaration-type-specifier expression '=' identifier ';'
+    ;
 
+    declaration-type-specifier
+    : 'const'
+    | 'let'
+    ;
+
+    expression
+    : identifier
+    | '[' array-struct
+    | '{' object-struct
+    ;
+
+    array-struct
+    : '[' expression (',' expression)* ']'
+    ;
+
+    object-struct
+    : '{' keyStruct (',' keyStruct)* '}'
+    ;
+
+    keyStruct
+    : identifier (':' expression)?
+    ;
+
+    identifier
+    : [a-zA-Z_][a-zA-Z_0-9]*
+    ;
+
+ */
+    // class Token {
+    //     public String kind;
+    //     public String lexeme;
+    //     public Token(String kind, String lexeme){
+    //         this.kind = kind;
+    //         this.lexeme = lexeme;
+    //     }
+    // }
+    public static ArrayList<Token> tokens;
+    public static int index;
+    public static Token lookahead;
+
+    public static Token nextToken() {
+            if (index >= tokens.size()) {
+                return new Token("EOF", "<EOF>");
+            } else {
+                return tokens.get(index++);
+            }
+    }
+    public static boolean peek(String kind) {
+        return lookahead.kind.equals(kind);
+    }
+
+    public static void consume(String kind) {
+        if (peek(kind)) {
+            lookahead = nextToken();
+        } else {
+            System.err.println("Expecting " + kind + " at " + lookahead.kind);
+            System.exit(1);
+        }
+    }
+
+    public static ArrayList<Object> program() {
+        ArrayList<Object> values = new ArrayList<>();
+        while (!peek("EOF")) {
+            values.add(declaration());
+        }
+        return values;
+    }
+
+    public static Object declaration() {
+        Object val1 = declarationTypeSpecifier();
+        Object val2 = expression();
+        if(peek("=")) {
+            consume("=");
+            Object val3 = identifier();
+            if(peek(";")){
+                consume(";");
+                val1 += val2 + "=" + val3 + ";";
+            }
+            else {
+                System.exit(0);
+            }
+        }
+        else {
+            System.exit(0);
+        }
+        return val1;
+    }
+
+    public static Object declarationTypeSpecifier() {
+        if(peek("RESERVED")){
+            Object val = lookahead.lexeme;
+            consume(lookahead.lexeme);
+            return val;
+        }
+        else {
+            System.exit(0);
+            return 0;
+        }
+    }
+    public static Object expression() {
+        if(peek("IDENTIFIER")){
+            return identifier();
+        }
+        else if(peek("[")){
+            return arrayStruct();
+        }
+        else if(peek("{")){
+            return objectStruct();
+        }
+        else {
+            System.exit(0);
+            return 0;
+        }
+    }
+    public static Object arrayStruct(){
+        if(peek("[")){
+            String val1 = lookahead.lexeme;
+            consume("[");
+            Object val2 = expression();
+            while(peek(",")){
+                String kind = lookahead.kind;
+                consume(kind);
+                val2 += kind + expression();
+            }
+            if(peek("]")){
+                String val3 = lookahead.lexeme;
+                consume("]");
+                return val1 += val2 + val3;
+            }
+            else {
+                return 0;
+            }
+        }
+        else{
+            return 0;
+        }
+    }
+    public static Object objectStruct(){
+        if(peek("{")){
+            String val1 = lookahead.lexeme;
+            consume("{");
+            Object val2 = keyStruct();
+            while(peek(",")){
+                String kind = lookahead.kind;
+                consume(kind);
+                val2 += kind + keyStruct();
+            }
+            if(peek("}")){
+                String val3 = lookahead.lexeme;
+                consume("}");
+                return val1 += val2 + val3;
+            }
+            else {
+                return 0;
+            }
+        }
+        else{
+            return 0;
+        }
+    }
+    public static Object keyStruct() {
+        if(peek("IDENTIFIER")){
+            Object val1 = identifier();
+            consume("IDENTIFIER");
+            if(peek(":")){
+                String val2 = lookahead.lexeme;
+                consume(":");
+                return val1 += val2 + expression();
+            }
+            else {
+                return identifier();
+            }
+        }
+        else {
+            return 0;
+        }
+    }
+    public static Object identifier(){
+        return lookahead.lexeme;
+    }
+    
+    public static Object parse(String text) {
+        tokens = scan(text);
+        index = 0;
+        lookahead = nextToken();
+        ArrayList<Object> value = program();
+        return value;
+    }
+
+    public static ArrayList<Token> scan(String text) {
+        ArrayList<Token> tokens = new ArrayList<>();
+
+        Pattern declarTypeSpecPattern = Pattern.compile("\\b(const|let)\\b");
+        Pattern identifierPattern = Pattern.compile("[a-zA-Z_][a-zA-Z_0-9]*");
+        Pattern ignoreSpaceComm = Pattern.compile("^(\\s*|\\s*//.*)");
+        Pattern singleCharPattern = Pattern.compile("[=;,{}\\[\\]]");
+        // Pattern singleCharPattern = Pattern.compile("^(.)");
+        // Pattern objectStructPattern = Pattern.compile("\\{" + identifierPattern.pattern() + "\\s*:\\s*(identifier|" + arrayStructPattern.pattern() + "|" + objectStructPattern.pattern() + ")(," + identifierPattern.pattern() + "\\s*:\\s*(identifier|" + arrayStructPattern.pattern() + "|" + objectStructPattern.pattern() + "))*\\}");
+        Pattern symbolPattern = Pattern.compile("[=;,{}\\[\\]]"); 
+
+        Matcher matcher = Pattern.compile("(" +
+            declarTypeSpecPattern.pattern() + "|" +
+            ignoreSpaceComm.pattern() + "|" +
+            identifierPattern.pattern() + "|" +
+            singleCharPattern.pattern() + "|\\s+"
+            + ")").matcher(text);
+
+        while (matcher.find()) {
+            //String lexeme = matcher.group().trim();
+            String lexeme = matcher.group();
+            String kind = "";
+            if (ignoreSpaceComm.matcher(lexeme).matches()){
+                continue;
+            } else if (declarTypeSpecPattern.matcher(lexeme).matches()) {
+                //kind = "DECLARATION_TYPE_SPECIFIER";
+                kind = "RESERVED";
+            } else if (identifierPattern.matcher(lexeme).matches()) {
+                kind = "IDENTIFIER"; //ID
+            } else if (singleCharPattern.matcher(lexeme).matches()) {
+                lexeme = matcher.group(1);
+                kind = lexeme;
+            } else {
+                throw new IllegalArgumentException("Invalid token: " + lexeme);
+            }
+            
+            tokens.add(new Token(kind, lexeme));
+        }
+        return tokens;
+    }
 
 
 
@@ -21,6 +275,7 @@ public class Parser {
         String filePath = f.getAbsolutePath();
         File file = new File(filePath);
         Scanner scanner = null;
+        String text = ""; //the whole file input into one string
         try {
             scanner = new Scanner(file);
         } catch (FileNotFoundException e) {
@@ -28,5 +283,14 @@ public class Parser {
         }
         while (scanner.hasNextLine()) {
              String line = scanner.nextLine();
+             text += line + "\n";
+        }
+        ArrayList<Token> tokens = scan(text);
+        for (Token token : tokens) {
+            System.out.println(token);
+        }
+        Object value = parse(text);
+        JSONObject jsonObject = new JSONObject();
+        // jsonObject.put("key", value);
     }
 }
--- submit/prj1-sol/make.sh
+++ submit/prj1-sol/make.sh
@@ -6,6 +6,8 @@ dir=`dirname $0`
 #use $dir to access programs in this directory
 #so that this script can be run from any directory.
 
-echo "no build done"
-
+#echo "no build done"
+export CLASSPATH=.:json-simple-1.1.1.jar
+javac Parser.java
+java Parser ~/cs471/projects/prj1/extras/tests/20-simple-array.test
 
